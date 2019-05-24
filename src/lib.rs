3571eff7bc2a39e8a3a3eb2034b3956bde45dc61
Alwin Berger <alwin.berger@cs.tu-dortmund.de>
2019-05-24 12:28:42 +0200

    Implement WavletTreeNode::new

--- src/lib.rs
+++ src/lib.rs
@@ -30,19 +30,45 @@ impl WavletTree {
 }
 
 struct WavletTreeNode {
-    bit_vec: BitVec<u8>,
-    rs: RankSelect,
+    bit_vec: RankSelect,
     left_child: Box<Option<WavletTreeNode>>,
     right_child: Box<Option<WavletTreeNode>>,
 }
 impl WavletTreeNode {
-    fn new(string: Vec<char>, alphabet: Vec<char>) -> Option<WavletTreeNode>{
-        //split alphabet
-        //create bitmap of string lenth
-        //assign bitmap 0/1s and create substrings
-        //create rankselect structure
-        //recusivley create left/right child from substring and partial alphabet
-        None
+    fn new(string: Vec<char>, alphabet: &Vec<char>) -> Option<WavletTreeNode>{
+        if alphabet.len() > 2 {
+            //split alphabet
+            let string_length = string.len();
+            let (l_a,r_a) = alphabet.split_at(alphabet.len()/2);
+            let mut left_alphabet = l_a.to_vec();
+            let mut right_alphabet = r_a.to_vec();
+            //create bitmap of string lenth
+            let mut bitvector : BitVec<u8>= BitVec::with_capacity(string_length as u64);
+            let mut left_string : Vec<char>= Vec::new();
+            let mut right_string : Vec<char>= Vec::new();
+            for i in (0..string_length) {
+                //assign bitmap 0/1s
+                let c = string.get(i).unwrap();
+                let bl = r_a.contains(c);
+                bitvector.set_bit(i as u64, bl);
+                //fill partial strings
+                if bl{
+                    right_string.push(*c);
+                }else{
+                    left_string.push(*c);
+                }
+            }
+            //create rankselect structure
+            let rs = RankSelect::new(bitvector, 1);
+            Some(WavletTreeNode{
+                bit_vec: rs,
+                //recusivley create left/right child from substring and partial alphabet
+                left_child: Box::new(WavletTreeNode::new(left_string,&left_alphabet)),
+                right_child: Box::new(WavletTreeNode::new(right_string,&right_alphabet)),
+            })
+        }else{
+            None
+        }
     }
     fn select(position: u32, alphabet: Vec<char>) -> char{
         //switch on 0/1
