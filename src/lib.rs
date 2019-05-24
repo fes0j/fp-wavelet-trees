58c7bb5a1ab917b333b2e6494a5ced9ecc8b9a17
fes0j <fes0j@users.noreply.github.com>
2019-05-24 10:52:04 +0200

    Fix typo and reformat

--- src/lib.rs
+++ src/lib.rs
@@ -3,40 +3,41 @@ use bv::BitVec;
 use bv::BitsMut;
 
 
-pub struct WavletTree {
+pub struct WaveletTree {
     //binaryTree
     //alpahbet
 }
 
-impl WavletTree {
-    pub fn new(placeholder: &str){
+impl WaveletTree {
+    pub fn new(placeholder: &str) {
         //new tree from parameter
     }
-    pub fn deserialize(placeholder: &str){
+    pub fn deserialize(placeholder: &str) {
         //deserialize
     }
-    pub fn serialize(&self){
+    pub fn serialize(&self) {
         //serialize
     }
-    pub fn access(position: u32){
+    pub fn access(position: u32) {
         //resolve character at position
     }
-    pub fn select(character: char, n: u32){
+    pub fn select(character: char, n: u32) {
         //return position of n-th character
     }
-    pub fn rank(character: char, n: u32){
+    pub fn rank(character: char, n: u32) {
         //number of characters until position n
     }
 }
 
-struct WavletTreeNode {
+struct WaveletTreeNode {
     bit_vec: BitVec<u8>,
     rs: RankSelect,
-    left_child: Box<Option<WavletTreeNode>>,
-    right_child: Box<Option<WavletTreeNode>>,
+    left_child: Box<Option<WaveletTreeNode>>,
+    right_child: Box<Option<WaveletTreeNode>>,
 }
-impl WavletTreeNode {
-    fn new(string: Vec<char>, alphabet: Vec<char>) -> Option<WavletTreeNode>{
+
+impl WaveletTreeNode {
+    fn new(string: Vec<char>, alphabet: Vec<char>) -> Option<WaveletTreeNode> {
         //split alphabet
         //create bitmap of string lenth
         //assign bitmap 0/1s and create substrings
@@ -44,7 +45,7 @@ impl WavletTreeNode {
         //recusivley create left/right child from substring and partial alphabet
         None
     }
-    fn select(position: u32, alphabet: Vec<char>) -> char{
+    fn select(position: u32, alphabet: Vec<char>) -> char {
         //switch on 0/1
         //newpos=rank 0/1
         //split alphabet
@@ -58,4 +59,15 @@ mod tests {
     use super::*;
 
     //TODO: write tests
+
+
+    //Test with 4 letters
+    #[ignore]
+    #[test]
+    fn test_deserialize() {
+        let test_string = "abacdc";
+        let w_tree = WaveletTree {};
+        WavltTree::deserialize(&test_string);
+        assert_eq!(0, 0);
+    }
 }
