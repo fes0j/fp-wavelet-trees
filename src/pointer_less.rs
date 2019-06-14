66d49084a1f4c1284d61ce53e86fff6d27022e01
Florian Hageneier <f.hageneier@gmail.com>
2019-06-14 12:28:52 +0200

    Fix iterators not being consumed and the result of concat not being used

--- src/pointer_less.rs
+++ src/pointer_less.rs
@@ -1,6 +1,7 @@
 use bio::data_structures::rank_select::RankSelect;
 use bv::BitVec;
 use bv::BitsMut;
+use bv::Bits;
 use bv::BitsExt;
 use itertools::Itertools;
 use serde::{Deserialize, Serialize};
@@ -14,21 +15,36 @@ use std::fmt::Debug;
 pub struct WaveletTreeCompact<T: PartialEq + Copy> {
     alphabet: Vec<T>,
     bit_vec: RankSelect,
+    sequence_len: u64,
 }
 
-/*impl<T: PartialEq + Copy> WaveletTree<T> for WaveletTreeCompact<T> {
+impl<T: PartialEq + Copy> WaveletTree<T> for WaveletTreeCompact<T> {
+    fn new(vector: impl Iterator<Item=T>) -> Self {
+        unimplemented!()
+    }
 
-}*/
+    fn access(&self, position: u64) -> Option<T> {
+        unimplemented!()
+    }
+
+    fn select(&self, object: T, n: u64) -> Option<u64> {
+        unimplemented!()
+    }
+
+    fn rank(&self, object: T, n: u64) -> Option<u64> {
+        unimplemented!()
+    }
+}
 
 impl<T: PartialEq + Copy> WaveletTreeCompact<T> {
     pub fn new(input: Vec<T>) -> WaveletTreeCompact<T> {
         //Create alphabet
         let mut alphabet: Vec<T> = Vec::new();
-        input.iter().map(|x| {
+        input.iter().foreach(|x| {
             if !alphabet.contains(&x) {
                 alphabet.push(*x);
             }
-        }).count();
+        });
 
         //Create vector for levels
         let mut levels: Vec<BitVec<u8>> = Vec::new();
@@ -37,14 +53,15 @@ impl<T: PartialEq + Copy> WaveletTreeCompact<T> {
         WaveletTreeCompact::create_bitvec(0, &mut levels, &input[..], &alphabet[..]);
 
         //Append all the levels into one big bitvec
-        let bit_vec:BitVec<u8> = BitVec::new();
+        let mut bit_vec:BitVec<u8> = BitVec::new();
         for l in levels {
-            bit_vec.bit_concat(l);
+            bit_vec = bit_vec.bit_concat(l).to_bit_vec();
         }
 
         WaveletTreeCompact {
             alphabet: alphabet.to_owned(),
-            bit_vec: RankSelect::new(bit_vec, 1),
+            bit_vec: RankSelect::new(bit_vec, crate::SUPERBLOCK_SIZE),
+            sequence_len: input.len() as u64,
         }
     }
 
@@ -53,15 +70,16 @@ impl<T: PartialEq + Copy> WaveletTreeCompact<T> {
             //Split alphabet
             let (left_alphabet, right_alphabet) = alphabet.split_at(
                 2usize.pow(
-                    ((alphabet.len() + 1) as f64).ln().ceil() as u32
-                )
+                    ((alphabet.len() + 1) as f64).log2().ceil() as u32 -1
+                ) -1
             );
+
             let mut l_seq = Vec::new();
             let mut r_seq = Vec::new();
             let mut local_bitvec = BitVec::new();
 
             //Fill left/right sequence and create bitvector for local "node"
-            sequence.iter().map(|x| {
+            sequence.iter().foreach(|x| {
                 if left_alphabet.contains(x) {
                     l_seq.push(*x);
                     local_bitvec.push(false);
@@ -69,7 +87,7 @@ impl<T: PartialEq + Copy> WaveletTreeCompact<T> {
                     r_seq.push(*x);
                     local_bitvec.push(true);
                 }
-            }).count();
+            });
 
 
             //Append to level bitvec
@@ -77,12 +95,30 @@ impl<T: PartialEq + Copy> WaveletTreeCompact<T> {
                 //Create new for this level
                 levels.push(BitVec::new());
             }
-            levels[level].bit_concat(local_bitvec);
+            levels[level] = levels[level].bit_concat(local_bitvec).to_bit_vec();
 
             //Call recursively for childs
             WaveletTreeCompact::create_bitvec(level + 1, levels, &l_seq[..], left_alphabet); //Left needs to be first!!
             WaveletTreeCompact::create_bitvec(level + 1, levels, &r_seq[..], right_alphabet);
-        } else {}
+        } else {
+            let (left_alphabet, right_alphabet) = alphabet.split_at(
+                2usize.pow(
+                    ((alphabet.len() + 1) as f64).log2().ceil() as u32 -1
+                ) -1
+            );
+            let mut local_bitvec = Bitvec::new();
+            sequence.iter().foreach(|x| {
+                if left_alp
+            });
+        }
+    }
+
+    fn rank_0(&self, l: u64, r: u64) -> Option<u64> {
+        Some(self.bit_vec.rank_0(r)? - self.bit_vec.rank_0(l)?)
+    }
+
+    fn rank_1(&self, l: u64, r: u64) -> Option<u64> {
+        Some(self.bit_vec.rank_1(r)? - self.bit_vec.rank_1(l)?)
     }
 }
 
@@ -142,9 +178,21 @@ mod tests {
     fn test_new_pointer_free(){
         let w_tree = WaveletTreeCompact::from("alabar_a_la_alabarda");
 
-
+        //TODO compare it to expected value
         assert_eq!(w_tree, w_tree);
+        //TEMP just for debug purposes
         println!("{:?}", w_tree);
 
+    }
+
+    #[test]
+    fn test_access_pointer_free(){
+        let test_string = "Hello world";
+        let w_tree = WaveletTreeCompact::from(test_string);
+
+        for (i, c)  in test_string.chars().enumerate() {
+            //assert_eq!(w_tree.access(i as u64), Some(c));
+            println!("Access: {}", w_tree.access(i as u64).unwrap());
+        }
     }
 }
