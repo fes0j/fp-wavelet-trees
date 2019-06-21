6f156baa8f0bd630a29cb4140b1fbfa66620027a
Florian Hageneier <f.hageneier@gmail.com>
2019-06-21 08:13:09 +0200

    Implement access

--- src/pointer_less.rs
+++ src/pointer_less.rs
@@ -24,9 +24,51 @@ impl<T: PartialEq + Copy> WaveletTree<T> for WaveletTreeCompact<T> {
     }
 
     fn access(&self, position: u64) -> Option<T> {
-        unimplemented!()
-    }
+        if position < self.sequence_len {
 
+            let mut l = 0u64;
+            let mut r = self.sequence_len-1;
+            let mut position = position;
+            let mut alphabet = &self.alphabet[..];
+            loop {
+                if l == r { //Reached end, return
+                    return Some(alphabet[0]);
+                }
+
+                if self.bit_vec.bits()[position] { //Right child
+                    let child_l = self.sequence_len + l;
+                    let child_r = self.sequence_len + l + self.rank_0(l, r).unwrap() -1;
+
+                    position = self.rank_1(l, l+position).unwrap();
+                    l = child_l;
+                    r = child_r;
+                    let (_, right_alphabet) = alphabet.split_at(
+                        2usize.pow(
+                            ((alphabet.len() + 1) as f64).ln().ceil() as u32 -1
+                        ) -1
+                    );
+                    alphabet = right_alphabet;
+
+                } else { //Left child
+                    let child_l = self.sequence_len + l + self.rank_0(l, r).unwrap();
+                    let child_r = self.sequence_len + r;
+
+                    position = self.rank_0(l, l+position).unwrap();
+                    l = child_l;
+                    r = child_r;
+                    let (left_alphabet, _) = alphabet.split_at(
+                        2usize.pow(
+                            ((alphabet.len() + 1) as f64).ln().ceil() as u32 -1
+                        ) -1
+                    );
+                    alphabet = left_alphabet;
+                }
+            }
+        } else {
+            None
+        }
+    }
+    
     fn select(&self, object: T, n: u64) -> Option<u64> {
         unimplemented!()
     }
@@ -51,6 +93,9 @@ impl<T: PartialEq + Copy> WaveletTreeCompact<T> {
 
         //Create bitvecs for levels
         WaveletTreeCompact::create_bitvec(0, &mut levels, &input[..], &alphabet[..]);
+        for l in levels.clone() {
+            println!("new: {:?}", l);
+        }
 
         //Append all the levels into one big bitvec
         let mut bit_vec:BitVec<u8> = BitVec::new();
@@ -106,10 +151,26 @@ impl<T: PartialEq + Copy> WaveletTreeCompact<T> {
                     ((alphabet.len() + 1) as f64).log2().ceil() as u32 -1
                 ) -1
             );
-            let mut local_bitvec = Bitvec::new();
+            let mut local_bitvec = BitVec::new();
             sequence.iter().foreach(|x| {
-                if left_alp
+                if left_alphabet.contains(x) {
+                    local_bitvec.push(false);
+                }else{
+                    local_bitvec.push(true);
+                }
             });
+
+            //Append to level bitvec
+            if levels.get(level).is_none() {
+                levels.push(BitVec::new());
+            }
+
+
+            /*if !levels.len() > level {
+                //Create new for this level
+                levels.push(BitVec::new());
+            }*/
+            levels[level] = levels[level].bit_concat(local_bitvec).to_bit_vec();
         }
     }
 
