f4ecee4fe387ab2518d1ec0848bb5ba6c9a6aec1
fes0j <fes0j@users.noreply.github.com>
2019-07-05 12:11:34 +0200

    Add Benchmark

---
+++ benches/benchmark.rs
@@ -0,0 +1,69 @@
+
+#[macro_use]
+extern crate criterion;
+extern crate fp_wavelet_trees;
+
+use criterion::{Criterion, ParameterizedBenchmark, Benchmark};
+use criterion::black_box;
+use fp_wavelet_trees::graph_wt;
+use fp_wavelet_trees::graph_wt::*;
+use fp_wavelet_trees::wavelet_tree_compact::*;
+use fp_wavelet_trees::wavelet_tree_pointer_based::*;
+
+
+
+fn graph_bench_point_build(n: usize) {
+
+   let mut wt_graph_builder = WTGraphBuilder::with_capacities(n);
+    for i  in 0..(n-2) {
+        wt_graph_builder.add_edge(i as u64,(i+1) as u64 );
+        wt_graph_builder.add_edge(i as u64,(i+2) as u64 );
+    }
+    let mut graph = wt_graph_builder.to_graph::<WaveletTreePointer<u64>>();
+    graph.neighbor(0,1);
+
+}
+
+fn graph_bench_point( mut graph:  WaveletTreeGraph<WaveletTreePointer<u64>>) {
+    for i in 0..300 {
+        graph.neighbor(0, 1);
+    }
+}
+fn graph_bench_compact(n: usize) {
+    let mut wt_graph_builder = WTGraphBuilder::with_capacities(n);
+    for i  in 0..(n-2) {
+        wt_graph_builder.add_edge(i as u64,(i+1) as u64 );
+        wt_graph_builder.add_edge(i as u64,(i+2) as u64 );
+    }
+    let mut graph = wt_graph_builder.to_graph::<WaveletTreeCompact<u64>>();
+    graph.neighbor(0,1);
+
+}
+
+fn bench_graph_build(c: &mut Criterion) {
+    c.bench(
+        "Graph",
+        Benchmark::new("Compact", |b| b.iter(|| graph_bench_compact(400)))
+            .with_function("Pointer", |b| b.iter(|| graph_bench_point_build(400))),
+    );
+}
+
+fn bench_graph_neighbor(c: &mut Criterion) {
+    let n : u64 = 500;
+    let mut wt_graph_builder = WTGraphBuilder::with_capacities(n as usize);
+    for i  in 0..(n-2) {
+        wt_graph_builder.add_edge(i as u64,(i+1) as u64 );
+        wt_graph_builder.add_edge(i as u64,(i+2) as u64 );
+    }
+    let mut graphP = wt_graph_builder.to_graph::<WaveletTreePointer<u64>>();
+    let mut graphC = wt_graph_builder.to_graph::<WaveletTreeCompact<u64>>();
+
+
+    c.bench(
+        "Graph",
+        Benchmark::new("Compact_neig", | b | b.iter(|| graph_bench_point(graphP ))),
+    );
+}
+
+criterion_group!(benches, bench_graph_build);
+criterion_main!(benches);
